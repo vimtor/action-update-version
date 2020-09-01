a7bc3eb2e53232ade4d8e35c27783873b0f0099b
Victor Navarro <victor@vnavarro.dev>
2020-09-01 21:21:04 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+<div align="center">
+   <h1>☝ update-version</h1>
+   <p>Update your files version field on new releases</p>
+</div>
+
+## 🧠 Why
+
+- Most actions related to version upgrade do it backwards:
+when you push a commit with version change in a specific branch a release is created.
+
+- This action does the opposite: when you create a release the
+specified files will get updated with the new version, so you don't forget to update them.
+
+This comes in handy when working with git workflows such as [trunk-base-development](https://trunkbaseddevelopment.com/) or [master-only](https://www.youtube.com/watch?v=MWz-9uyHP4s).
+
+## 🚀 Usage
+
+With the following example after creating a new release with tag `v2.0.1` on branch `release`,
+a new commit will appear in that same branch with both `package.json` and `app.yaml` updated
+with the version field to `2.0.1`.
+
+```yaml
+name: Upgrade Version
+on:
+  release:
+    types: [published]
+
+jobs:
+  upgrade-version:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: pocket-studios/action-update-version@v1
+        with:
+          files: 'package.json, app.yaml'
+          version-regexp: '\d+.\d+.\d+'
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+The action will fail if:
+- Both `repo-token` and `branch-name` are not supplied
+- The tag cannot be found by `octokit`
+- The regular expression cannot match the release tag
+- You specify a file with unsupported extension
+
+
+## ⚙ Inputs
+
+By supplying the `repo-token` the commit will use the release information: author and branch.
+
+You can change the branch commit target and the commit author if you want.
+
+**Name**|**Description**|**Default**
+-----|-----|-----
+files|Comma separated list of files to update its version field|package.json
+version-regexp|Regular expression to match release tag name|\d+.\d+.\d+
+repo-token|GitHub token to get the release information in order to push to branch|`null` 
+commit-message|Commit message for files update. The %version% will get substituted|ci: update version to v%version%
+spacing-level|Number of spaces for formatted files|`2`
+branch-name|Default branch name to push changes if not repo-token is provided|*Release target branch* 
+author-name|Commit author name|*Release author name* 
+author-email|Commit author email|*Release author email* 
+
+## 👋 Support
+
+- Supported file extensions: `json`, `yaml` and `yml`. To add one simply submit a PR with a new parser on the `main.ts` file.
---
+++ src/main.ts
@@ -0,0 +1,113 @@
+import path from 'path';
+import fs from 'fs';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
+import YAML from 'yaml';
+
+const getParser = (file: string, options: { spacing: number }) => {
+  const extension = path.extname(file).replace('.', '');
+
+  switch (extension) {
+    case 'json':
+      return {
+        read: JSON.parse,
+        write: (data: any) => JSON.stringify(data, null, options.spacing),
+      };
+    case 'yaml':
+    case 'yml':
+      return {
+        read: YAML.parse,
+        write: (data: any) => YAML.stringify(data, { indent: options.spacing }),
+      };
+    default:
+      throw new Error(`Unsupported file extension "${extension}".\nTo add it you can simply submit a PR adding a new parser.`);
+  }
+};
+
+const run = async () => {
+  core.info('Setting input and environment variables');
+  const root = process.env.GITHUB_WORKSPACE as string;
+  const tag = (process.env.GITHUB_REF as string).replace('refs/tags/', '');
+  const token = core.getInput('repo-token');
+  const regex = new RegExp(core.getInput('version-regexp'));
+  const files = core.getInput('files').replace(' ', '').split(',');
+  const message = core.getInput('commit-message');
+  const spacing = parseInt(core.getInput('spacing-level'), 10);
+  let branch = core.getInput('branch-name');
+  let author = core.getInput('author-name');
+  let email = core.getInput('author-email');
+
+  if (!token && !branch) {
+    throw new Error('Either repo-token or branch-name must be supplied.');
+  }
+
+  if (token) {
+    core.info('Setting up Octokit and context');
+    const octokit = github.getOctokit(token);
+    const { owner, repo } = github.context.repo;
+
+    core.info('Fetching repo task history');
+    const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
+
+    if (release.data.tag_name !== tag) {
+      throw new Error(`Release with name "${tag}" was not found`);
+    }
+
+    branch = release.data.target_commitish;
+
+    if (!author && !email) {
+      core.info('Getting author and email from release information');
+      const username = release.data.author.login;
+      const user = await octokit.users.getByUsername({ username });
+
+      author = user.data.name;
+      email = user.data.email;
+      core.info(`👋 Nice to meet you ${author} (${email})!`);
+    }
+  } else {
+    core.info('Skipping getting the latest release since not "repo-token" was provided');
+  }
+
+  core.info(`Checking latest tag "${tag}" against input regexp`);
+  if (!regex.test(tag)) {
+    throw new Error(`RegExp could not be matched to latest tag "${tag}"`);
+  }
+
+  const version = (tag.match(regex) as string[])[0];
+  core.info(`Extracted new version "${version}" from "${tag}"`);
+
+  // Forgive me for the unnecessary fanciness 🙏
+  core.info('Updating files version field');
+  const changed = files.reduce((change, file) => {
+    const dir = path.join(root, file);
+    const buffer = fs.readFileSync(dir, 'utf-8');
+    const parser = getParser(file, { spacing });
+    const content = parser.read(buffer);
+
+    if (content.version === version) {
+      core.info(`  - ${file}: Skip since equal versions`);
+      return change;
+    }
+
+    core.info(`  - ${file}: Update version from "${content.version}" to "${version}"`);
+    content.version = version;
+    fs.writeFileSync(dir, parser.write(content));
+    return true;
+  }, false);
+
+  if (!changed) {
+    core.info('Skipped commit since no files were changed');
+    return;
+  }
+
+  core.info('Committing file changes');
+  await exec.exec('git', ['config', '--global', 'user.name', author]);
+  await exec.exec('git', ['config', '--global', 'user.email', email]);
+  await exec.exec('git', ['commit', '-am', message.replace('%version%', version)]);
+  await exec.exec('git', ['push', '-u', 'origin', `HEAD:${branch}`]);
+};
+
+run()
+  .then(() => core.info('Updated files version successfully'))
+  .catch(error => core.setFailed(error.message));
